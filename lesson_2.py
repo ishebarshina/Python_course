b271405bea01f2d5420cdae0fd1e745f9f59cdf0
Irina Shebarshina <iv.shebarshina@gmail.com>
2021-09-09 19:16:36 +0300

    lesson2 done

--- lesson_2.py
+++ lesson_2.py
@@ -23,7 +23,6 @@ for i in var_list_1:
 # При нечетном количестве элементов последний сохранить на своем месте.
 # Для заполнения списка элементов необходимо использовать функцию input().
 
-# var_list_2 = list(input(""))
 from ast import literal_eval
 
 
@@ -54,5 +53,114 @@ for elem in var_list_2:
     print(f"element: {elem} \t type: {type(elem)}")
 
 # %%
+
+# 3. Пользователь вводит месяц в виде целого числа от 1 до 12.
+# Сообщить к какому времени года относится месяц (зима, весна, лето, осень).
+# Напишите решения через list и через dict.
+
+n_month = int(input("Введите целое число от 1 до 12: "))
+
+# via list
+if (n_month in [1, 2, 12]):
+    print("Winter")
+elif (n_month in [3, 4, 5]):
+    print("Spring")
+elif (n_month in [6, 7, 8]):
+    print("Summer")
+elif (n_month in [9, 10, 11]):
+    print("Autumn")
+else:
+    print("Incorrect number of month")
+
+# via dict
+month_dict = {'Winter': [1, 2, 12], 'Spring': [3, 4, 5],
+              'Summer': [6, 7, 8], 'Autumn': [9, 10, 11]}
+
+if (n_month in month_dict['Winter']):
+    print("Winter")
+elif (n_month in month_dict['Spring']):
+    print("Spring")
+elif (n_month in month_dict['Summer']):
+    print("Summer")
+elif (n_month in month_dict['Autumn']):
+    print("Autumn")
+else:
+    print("Incorrect number of month")
+
+# %%
+
+# 4. Пользователь вводит строку из нескольких слов, разделённых пробелами.
+# Вывести каждое слово с новой строки. Строки необходимо пронумеровать.
+# Если в слово длинное, выводить только первые 10 букв в слове.
+
+in_str = str(input("Enter string: "))
+# in_str = 'fgh ghj kl hnm yui 0123456789456'
+pr_str = in_str.split(' ')
+print("Номер строки: строка")
+for i in range(len(pr_str)):
+    print(f"{i}: {pr_str[i][:10]}")
+
+# %%
+
+# 5. Реализовать структуру «Рейтинг», представляющую собой
+# не возрастающий набор натуральных чисел.
+# У пользователя необходимо запрашивать новый элемент рейтинга.
+# Если в рейтинге существуют элементы с одинаковыми значениями,
+# то новый элемент с тем же значением должен разместиться после них.
+# Набор натуральных чисел можно задать непосредственно в коде,
+# например, my_list = [7, 5, 3, 3, 2].
+
+rating_list = [7, 5, 3, 3, 2]
+for i in range(3):
+    new_elem = int(input("Введите новый элемент рейтинга: "))
+    rating_list.append(new_elem)
+    rating_list.sort(reverse=True)
+    print(f"Рейтинг: {rating_list}")
+
+# %%
+
+# 6. *Реализовать структуру данных «Товары».
+# Она должна представлять собой список кортежей.
+# Каждый кортеж хранит информацию об отдельном товаре.
+# В кортеже должно быть два элемента — номер товара и словарь с параметрами
+# (характеристиками товара: название, цена, количество, единица измерения).
+# Структуру нужно сформировать программно,
+# т.е. запрашивать все данные у пользователя.
+
+# Необходимо собрать аналитику о товарах.
+# Реализовать словарь, в котором каждый ключ — характеристика товара,
+# например название, а значение — список значений-характеристик,
+# например список названий товаров.
+
+products = []
+for i in range(3):
+    name = str(input("Название: "))
+    price = float(input("Цена: "))
+    amount = int(input("Количество, штук: "))
+    products.append(tuple([i, {"Название": name, "Цена": price,
+                               "Количество": amount, "ед": "шт."}]))
+
+# [
+#     (1, {“название”: “компьютер”, “цена”: 20000, “количество”: 5, “eд”: “шт.”}),
+#     (2, {“название”: “принтер”, “цена”: 6000, “количество”: 2, “eд”: “шт.”}), 
+#     (3, {“название”: “сканер”, “цена”: 2000, “количество”: 7, “eд”: “шт.”})
+# ]
+
+products_keys = list(products[0][1].keys())
+analytics = {}
+for k in range(len(products_keys)):
+    analytics.update({products_keys[k]: []})
+for i in range(len(products)):
+    for k in range(len(products_keys)):
+            analytics[products_keys[k]].append(products[i][1][products_keys[k]])
+
+print("Analitics:")
+for i in analytics:
+    print(f"{i}: {analytics[i]}")
+
+
+
+
+
 
 
