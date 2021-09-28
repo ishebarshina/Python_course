caab005d6395e161db21b9af55a569f8573359e8
Irina Shebarshina <iv.shebarshina@gmail.com>
2021-09-28 08:11:41 +0300

    lesson 7_1 done

--- lesson_7/lesson_7_1.py
+++ lesson_7/lesson_7_1.py
@@ -10,20 +10,30 @@ class Matrix:
     """Class Matrix."""
 
     def __init__(self, list_data: list):
+        """Override constructor."""
         self.matrix = list_data
 
-    def get_matrix(self):
-        return self.matrix
-    
     def __str__(self):
-        # return str('\n'.join(['\t'.join([str(j) for j in i]) for i in self.matrix]))
-        return "here"
+        """Override method to print matrix."""
+        return '\n'.join('\t'.join(str(elem) for elem in line)
+                         for line in self.matrix)
+
+    def __add__(self, obj):
+        """Override method for addition. Returns Matrix object."""
+        matrix2 = []
+        for i in range(len(obj.matrix)):
+            temp = []
+            for j in range(len(obj.matrix[i])):
+                temp.append(obj.matrix[i][j] + self.matrix[i][j])
+            matrix2.append(temp)
+        return Matrix(matrix2)
 
 
-
-# %%
-
-x = Matrix([[1, 2, 3], [4, 5, 6]])
+a = [[1, 2, 3], [4, 5, 6]]
+b = [[7, 8, 9], [0, 1, 2]]
+x = Matrix(a)
+y = Matrix(b)
 print(x.__str__())
 print(x.matrix)
 print(x)
+print(x+y)
