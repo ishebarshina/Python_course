1ccfdfe769972a420f9aaab4e1a8e7172f90be97
Irina Shebarshina <iv.shebarshina@gmail.com>
2021-09-30 21:06:47 +0300

    done lesson8_2

--- lesson_8/lesson_8_2.py
+++ lesson_8/lesson_8_2.py
@@ -6,3 +6,42 @@
 программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
 """
 
+class OwnError(Exception):
+
+    @staticmethod
+    def division(num, denom):
+        try:
+            res = num / denom
+        except ZeroDivisionError:
+            print("Division by zero exception.")
+        else:
+            print(f"{num} / {denom} = {res}")
+
+
+OwnError.division(1, 0)
+
+# %% другой вариант 
+
+import sys
+
+class OwnError(Exception):
+    def __init__(self):
+        try:
+            self.num = float(input("Числитель: "))
+            self.denom = float(input("Знаменатель: "))
+        except Exception:
+            print("Incorrect input")
+            sys.exit()
+
+    @property
+    def division(self):
+        try:
+            self.res = self.num / self.denom
+        except ZeroDivisionError:
+            print("Division by zero exception.")
+        else:
+            print(f"{self.num} / {self.denom} = {self.res}")
+
+
+x = OwnError()
+x.division
